6e74a6decc867e70bc1dae1cd2b27d47984af533
BugFreeAnanya <anilmishra.bok17@gmail.com>
2025-12-31 17:40:36 +0530

    conditional statements

---
+++ natural-number.py
@@ -0,0 +1,8 @@
+number= int(input("Enter a number: "))
+print("The number to be checked is:", number)
+if number>0:
+    print("It is a natural number")
+elif number==0:
+    print("Zero is not a natural number")
+else:
+    print("Negative numbers are not natural numbers")
