b01f907ac5887d044b0bd6e5563ef0555b809235
BugFreeAnanya <anilmishra.bok17@gmail.com>
2026-01-04 15:52:01 +0530

    loops

---
+++ Natural numbers sum.py
@@ -0,0 +1,6 @@
+num = 1
+sum = 0
+while (num < 11):
+    sum = sum + num
+    num = num + 1
+print("The sum of first 10 natural numbers is:", sum)    
---
+++ Table of 17.py
@@ -0,0 +1,5 @@
+num= 17
+print("Table of 17")
+for i in range(1,11):
+    mul= num*i
+    print(f"{num}x{i}={mul}")
