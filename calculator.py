3e9ad2a387175436eb5e6d649e138c3b6b0a4f87
BugFreeAnanya <anilmishra.bok17@gmail.com>
2026-01-07 17:58:54 +0530

    functions in python

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition result :", add_numbers(num1, num2))
+print("Multiplication result :", multiply_numbers(num1, num2))
