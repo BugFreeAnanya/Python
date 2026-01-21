33cb171dcab3154321a640e46b8784b1dcfd81fd
BugFreeAnanya <anilmishra.bok17@gmail.com>
2026-01-21 18:09:08 +0530

    oop programming 2

---
+++ Circle Programming.py
@@ -0,0 +1,14 @@
+class Circle:
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+    
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+    
+    circle1 = Circle(10)
+    print(f"Radius of the Circle is: {circle1.radius}")
+    print("Area of the Circle is:", circle1.area())
+    print("Perimeter of the Circle is:", circle1.perimeter())
